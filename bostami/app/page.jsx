28be954bc6755cc99c3fa9654db0766793ea9c0c
rthakur1312 <67025716+rthakur1312@users.noreply.github.com>
2024-02-21 00:57:38 -0800

    Update page.jsx

--- bostami/app/page.jsx
+++ bostami/app/page.jsx
@@ -1,28 +1,24 @@
-import HeaderFour from "@/components/homes/headers/HeaderFour";
-import HeaderThree from "@/components/homes/headers/HeaderThree";
-import PersonalInfoThree from "@/components/homes/personalInfo/PersonalInfoThree";
-import ResumeThree from "@/components/homes/resume/ResumeThree";
-import React from "react";
+import Hero from "@/components/preview/Hero";
+import Buy from "@/components/preview/Buy";
+import CopyRight from "@/components/preview/CopyRight";
+import Demo from "@/components/preview/Demo";
+import Features from "@/components/preview/Features";
+
+import "../public/assets/css/style.css";
+
 export const metadata = {
-  title: "Rashi Thakur | Resume",
+  title: "Preview || Bostami - Professional portfolio NextJS Template",
   description:
     "Discover Botami,the most impressive portfolio template for work showcase, blog",
 };
 export default function page() {
   return (
     <>
-      <div className="page-wrapper home-3">
-        <HeaderFour />
-        <div className="container z-index-3">
-          <div className="row">
-            <PersonalInfoThree />
-            <div className="col-xxl-8 col-xl-8 col-lg-8">
-              <HeaderThree />
-              <ResumeThree />
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* <Hero /> */}
+      <Demo />
+      {/* <Features /> */}
+      {/* <Buy /> */}
+      {/* <CopyRight /> */}
     </>
   );
 }
