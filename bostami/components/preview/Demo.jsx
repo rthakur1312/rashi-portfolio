3c977a561531ec3a167975f533a7d47e67738557
rthakur1312 <67025716+rthakur1312@users.noreply.github.com>
2024-02-21 00:58:42 -0800

    Update Demo.jsx

--- bostami/components/preview/Demo.jsx
+++ bostami/components/preview/Demo.jsx
@@ -8,7 +8,7 @@ export default function Demo() {
     <div className="home-demo-area bg-black " id="demo">
       <div className="container">
         <div className="section-title-wrap text-center mb-70">
-          <h1 className="main-title">home demo</h1>
+          <h1 className="main-title">Portfolio</h1>
           <Image
             width={372}
             height={68}
@@ -16,14 +16,20 @@ export default function Demo() {
             src="/assets/img/preview.png"
             alt="shape"
           />
-          <h2 className="section-title">03 Awesome Light & Dark Demo</h2>
+          {/* <h2 className="section-title">03 Awesome Light & Dark Demo</h2> */}
           <p className="section-pera">
-            Bostami build with Modern, Elegant & Beautiful trendng
-            <br /> technology like React Nextjs template.
+          Welcome to my portfolio! Here, you'll find a curated collection 
+          of my work as a frontend developer. With a passion for crafting engaging 
+          and user-centric web experiences, I specialize in turning ideas into 
+          intuitive, visually appealing interfaces. Through a combination of 
+          creativity, technical expertise, and attention to detail, I strive to 
+          create seamless digital solutions that delight users and elevate brands. 
+          Join me on a journey through my projects, where innovation meets 
+          functionality, and let's build the web together.
           </p>
         </div>
 
-        <div className="row">
+        <div className="row justify-content-center">
           {homepages.map((elm, i) => (
             <div key={i} className="col-lg-4 col-md-6 col-sm-6">
               <Link
@@ -35,7 +41,7 @@ export default function Demo() {
                 <span>
                   <Image width={520} height={540} src={elm.imgSrc} alt="demo" />
                 </span>
-                <span className="demo-title">{elm.title}</span>
+                <span className="demo-title text-decoration-underline">{elm.title}</span>
               </Link>
             </div>
           ))}
